a812349afb3429d7a88e6881f29fb0f15303ab4f
Mike Wales <michael.wales@udacity.com>
2014-11-05 13:35:36 -0800

    Removed extraneous patterns var

--- js/engine.js
+++ js/engine.js
@@ -23,7 +23,6 @@ var Engine = (function(global) {
         win = global.window,
         canvas = doc.createElement('canvas'),
         ctx = canvas.getContext('2d'),
-        patterns = {},
         lastTime;
 
     canvas.width = 505;
