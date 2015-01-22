3525733ba5430fe05bed1a7110d91b9842ed96b1
Carlos Saraiva <carlos.saraiva@msn.com>
2015-01-22 19:15:48 +0000

    Revert "Player class and allEnemies array added"
    This reverts commit baa6451e7b97d0918c1e058b6250bee49f9ce9cf.

--- js/app.js
+++ js/app.js
@@ -24,20 +24,12 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function(){
 
-}
-Player.prototype.render = function(){
 
-}
-Player.prototype.update = function(){
-    
-}
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var allEnemies = [];
-var player = new Player();
+
 
 
 // This listens for key presses and sends the keys to your
