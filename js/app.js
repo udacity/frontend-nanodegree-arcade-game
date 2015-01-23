e112d3ff8dbc01df98fd5aa9950957226149a949
Carlos Saraiva <carlos.saraiva@msn.com>
2015-01-23 13:46:59 -0200

    Grid and PVector classes created

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
+var PVector = function(x, y){
+    this.x = x;
+    this.y = y;
+}
+
+var Grid = function(rows, cols){
+    this.rows = rows;
+    this.cols = cols;
+    this.position = new Array();
+    //cols * 101, row * 83
+
+    for(var i = 0; i < rows; i++){
+        this.position[i] = new Array();
+        for(var j = 0; j < cols; j++){
+            this.position[i][j] = new PVector(i * 101, j * 83);
+        }
+    }
+}
+
 //Superclass Entity wich will be common in Enemy and PLayer
 var Entity = function(x, y, speed, sprite) {
     this.x = x;
@@ -37,22 +56,22 @@ Player.prototype.constructor = Player;
 Player.prototype.handleInput = function(key) {
     console.log(key);
     if (key === 'right') {
-        this.x += this.speed;
+        this.x += 10;
         console.log("aqui");
     }
 
     if (key === 'left') {
-        this.x -= this.speed;
+        this.x -= 10;
         console.log("aqui");
     }
 
     if (key === 'up') {
-        this.y -= this.speed;
+        this.y -= 10;
         console.log("aqui");
     }
 
     if (key === 'down') {
-        this.y += this.speed;
+        this.y += 10;
         console.log("aqui");
     }
 }
@@ -68,12 +87,13 @@ Player.prototype.update = function(dt) {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var grid = new Grid(5, 6);
 var allEnemies = [];
 var player = new Player(0, 0, 5);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keydown', function(e) {
+document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
