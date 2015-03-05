51c8b08b82c595b064f0903df6d2b6d4cfb23e0b
Rita Morais <rita@tasboa.com>
2015-03-05 11:16:43 +0100

    Cat hit trial
    Tried to implement a new png for when he’s hit, but couldn’t =/
    need to time it maybe?

--- js/app.js
+++ js/app.js
@@ -129,8 +129,9 @@ player.prototype.win = function() {
 
 //when player looses looses 1 life and resets
 player.prototype.lose = function() {
+  // this.sprite = 'images/char-cat-hit.png';
+
   this.lives -= 1;
-  console.log(this.lives);
   allHearts[this.lives].sprite = "images/Heartless.png";
 
   if (this.lives > 0) {
@@ -175,6 +176,7 @@ function checkCollisions() {
     //due to sporadic strange rendering errors for the y value used 60 instead of 0
     if(Math.abs(bug.y-player.y)<60 && player.lives > 0){ //prevents player from having negative lives
       if(Math.abs(bug.x-player.x)<80) 
+
         player.lose();    
     }
   });
