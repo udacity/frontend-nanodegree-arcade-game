89ee43ab1bfd60a449f5b05887b75df55c5b93f7
Max Soe <maxsoe@gmail.com>
2015-07-20 20:16:45 +0100

    More progress
    TODO:
    - collision detection. Set which tile the character is currently in.
    The variation will be stored as a property of each bug/player object.
    The type will be [x,y].

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
+// Some variables to measure around the canvas
+var tileWidth = 101;
+var topArea = 60;
+var tileHeight = 83;
+
+// For debugging purposes
+function debugOutput(string, debugMode) { 
+  if (debugMode == 1) {
+    console.log(string);
+  }
+}
+
 // Enemies our player must avoid
 var Enemy = function() {
     // Variables applied to each of our instances go here,
@@ -6,6 +18,7 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = -110;
 }
 
 // Update the enemy's position, required method for game
@@ -14,74 +27,81 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    if (this.x < 550) {
+      this.x++;
+      debugOutput('Bugs are at ' +this.x, 0);
+    } else {
+      this.x = -110;
+    }
 }
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {;  
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
-
-
-
-var tileWidth = 101;
-var topArea = 60;
-var tileHeight = 80;
   
 // Now write your own player class
 var Player = function() {
   this.sprite = 'images/char-boy.png';
-  this.x = tileWidth*2;
-  this.y = topArea + tileHeight*2;
+  this.x = tileWidth*2; // Start on the third column
+  this.y = topArea + tileHeight*3; // Start on the forth row
 }
 
 // This class requires an update(), render() and
 // a handleInput() method.
 
-Player.prototype.update = function() {
-
+Player.prototype.update = function(direction) {
 }
 
 Player.prototype.render = function(direction) {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-  console.log('Player is at (' +this.x +"," +this.y +')');
+  debugOutput('Player is at (' +this.x +"," +this.y +')', 0);
 }
 
 Player.prototype.handleInput = function(direction) {
   if (direction == 'left') {
-    console.log("player pressed left");
-    console.log("this.x was at " +this.x +". tileWidth was " +tileWidth);
+    debugOutput("player pressed " +direction, 1);
+    debugOutput("this.x was at " +this.x +". tileWidth was " +tileWidth);
     if (this.x > 0 ) { // ensure that we don't go past the first column
       this.x = this.x - tileWidth;
-      console.log("valid move");
+      // Attempted animation
+/*
+      var lastFrame = (this.x - tileWidth);
+      if (this.x > lastFrame) {
+        this.x--; 
+      }
+*/
+      debugOutput("valid move");
     }
-    console.log("this.x is at " +this.x +". tileWidth is " +tileWidth);
+    debugOutput("this.x is at " +this.x +". tileWidth is " +tileWidth);
   }
+
   if (direction == 'up') {
-    console.log("player pressed up");
-    console.log("this.y was at " +this.y +". tileHeight was " +tileHeight);
+    debugOutput("player pressed " +direction, 1);
+    debugOutput("this.y was at " +this.y +". tileHeight was " +tileHeight);
     if (this.y > 0 ) { // ensure that we don't go past the first row
       this.y = this.y - tileHeight;
-      console.log("valid move");
+      debugOutput("valid move");
     }
-    console.log("this.y is at " +this.y +". tileHeight is " +tileHeight);
+    debugOutput("this.y is at " +this.y +". tileHeight is " +tileHeight);
   }
   if (direction == 'right') {
-    console.log("player pressed right");
-    console.log("this.x was at " +this.x +". tileWidth was " +tileWidth);
+    debugOutput("player pressed " +direction, 1);
+    debugOutput("this.x was at " +this.x +". tileWidth was " +tileWidth);
     if (this.x < tileWidth*4 ) { // ensure that we don't go past the last column
       this.x = this.x + tileWidth;
-      console.log("valid move");
+      debugOutput("valid move");
     }
-    console.log("this.x is at " +this.x +". tileWidth is " +tileWidth);
+    debugOutput("this.x is at " +this.x +". tileWidth is " +tileWidth);
   }
   if (direction == 'down') {
-    console.log("player pressed down");
-    console.log("this.y was at " +this.y +". tileHeight was " +tileHeight);
+    debugOutput("player pressed " +direction, 1);
+    debugOutput("this.y was at " +this.y +". tileHeight was " +tileHeight);
     if (this.y < (tileHeight*4 + topArea) ) { // ensure that we don't go past the bottow row
       this.y = this.y + tileHeight;
-      console.log("valid move");
+      debugOutput("valid move");
     }
-    console.log("this.y is at " +this.y +". tileHeight is " +tileHeight);
+    debugOutput("this.y is at " +this.y +". tileHeight is " +tileHeight);
   }
 }
 
@@ -90,8 +110,19 @@ Player.prototype.handleInput = function(direction) {
 // Instantiate each enemy
 // Rows are counted from the top
 var enemyTop = new Enemy();
+enemyTop.y = 0 + topArea;
+/*
+enemyTop.update = function() {
+  this.x+3;
+};
+*/
+
 var enemyMiddle = new Enemy();
+enemyMiddle.y = 0 + topArea + tileHeight;
+
 var enemyBottom = new Enemy();
+enemyBottom.y = 0 + topArea + tileHeight*2;
+
 
 // Place all enemy objects in an array called allEnemies
 var allEnemies = [enemyTop, enemyMiddle, enemyBottom];
