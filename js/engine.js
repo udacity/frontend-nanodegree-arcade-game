c87f9a919996ded8b0d8b51d25f7cf8bc0d7afbc
Ryan McAllen <rhyno@rhynodesigns.com>
2015-10-05 02:15:19 -0400

    Spelling: It's -> Its, developer's -> developers; Extra spaces; Extra empty line

--- js/engine.js
+++ js/engine.js
@@ -83,11 +83,11 @@ var Engine = (function(global) {
         // checkCollisions();
     }
 
-    /* This is called by the update function  and loops through all of the
+    /* This is called by the update function and loops through all of the
      * objects within your allEnemies array as defined in app.js and calls
      * their update() methods. It will then call the update function for your
      * player object. These update methods should focus purely on updating
-     * the data/properties related to  the object. Do your drawing in your
+     * the data/properties related to the object. Do your drawing in your
      * render methods.
      */
     function updateEntities(dt) {
@@ -136,12 +136,11 @@ var Engine = (function(global) {
             }
         }
 
-
         renderEntities();
     }
 
     /* This function is called by the render function and is called on each game
-     * tick. It's purpose is to then call the render functions you have defined
+     * tick. Its purpose is to then call the render functions you have defined
      * on your enemy and player entities within app.js
      */
     function renderEntities() {
@@ -177,7 +176,7 @@ var Engine = (function(global) {
     Resources.onReady(init);
 
     /* Assign the canvas' context object to the global variable (the window
-     * object when run in a browser) so that developer's can use it more easily
+     * object when run in a browser) so that developers can use it more easily
      * from within their app.js files.
      */
     global.ctx = ctx;
