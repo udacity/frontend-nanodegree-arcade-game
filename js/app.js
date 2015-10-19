ee90b6742b60f3f752a6ca7c8f091af7f0c199e8
dm4000 <daniel@listretto.com>
2015-10-19 13:58:12 +1100

    Clean comments

--- js/app.js
+++ js/app.js
@@ -1,14 +1,3 @@
-// Hello.
-//
-// This is JSHint, a tool that helps to detect errors and potential
-// problems in your JavaScript code.
-//
-// To start, simply enter some JavaScript anywhere on this page. Your
-// report will appear on the right side.
-//
-// Additionally, you can toggle specific options in the Configure
-// menu.
-
 // Enemies our player must avoid
 var Enemy = function (startX, startY, speed) {
   // Variables applied to each of our instances go here,
