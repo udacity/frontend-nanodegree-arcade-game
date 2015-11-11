71a06549842d5e14819b589e69f012ff94d27ca2
b-ritter <ben@benjaminritter.com>
2015-11-11 05:27:18 -0500

    Sketch out player select.

--- js/app.js
+++ js/app.js
@@ -354,9 +354,36 @@ var Scorekeeper = {
   }
 };
 
+var Player = function(sprite, x, y) {
+  this.x = x;
+  this.y = y;
+  this.sprite = sprite;
+};
+
+Player.prototype.render = function() {
+  // this.x
+  // this.y
+  ctx.canvas.drawImage(this.sprite);
+};
+
 var PlayerSelect = {
-  update: function(){
+  players: [
+    'images/char-boy.png',
+    'images/char-cat-girl.png',
+    'images/char-horn-girl.png',
+    'images/char-pink-girl.png',
+    'images/char-princess-girl.png'
+  ],
+  update: function() {
     // Hi
+  },
+  render: function() {
+    this.players.forEach(function(player){
+
+    });
+  },
+  handleInput: function() {
+    // Use the mouse to select player
   }
 };
 
--- js/engine.js
+++ js/engine.js
@@ -196,6 +196,10 @@ var Engine = (function(global) {
         'images/grass-block.png',
         'images/enemy-bug.png',
         'images/char-boy.png',
+        'images/char-cat-girl.png',
+        'images/char-horn-girl.png',
+        'images/char-pink-girl.png',
+        'images/char-princess-girl.png',
         'images/frog.png'
     ]);
     Resources.onReady(init);
