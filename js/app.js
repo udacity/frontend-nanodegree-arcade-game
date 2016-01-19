f95a92e3a5d132ba5a0ac80d5e470fc2d9a9a8ae
shawnajean <shawnajean@users.noreply.github.com>
2016-01-18 21:14:38 -0600

    Correct enemy's row randomization on reentry

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ Enemy.prototype.update = function(dt) {
     // which will ensure the game runs at the same speed for
     // all computers.
     this.x = (((this.x + 100) + (this.speed * dt)) % 600) - 100;
-    if( this.x > 600 ) {
+    if( this.x > 499 ) {
         this.set();
     }
 };
