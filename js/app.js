965525fbb7ba5ecda6bb2e2bc728fc3760b9e5ee
Pedro Rosario <pedrorosario.estrella@hotmail.com>
2016-02-15 14:36:32 -0400

    Game is now working

--- js/app.js
+++ js/app.js
@@ -2,7 +2,9 @@
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.x = 0;
+    this.y = ypositions[Math.floor(Math.random() * ypositions.length)];
+    
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -14,6 +16,12 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    movement = Math.floor((Math.random() * 250) + 150) * dt;
+    this.x += movement;
+    if (this.x > 505){
+        this.x = 0;
+        this.y = ypositions[Math.floor(Math.random() * ypositions.length)];
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,12 +32,62 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+    this.x = 200;
+    this.y = 300;
 
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/char-boy.png';
 
+};
+
+Player.prototype.update = function() {
+
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(key){
+    switch (key){
+        case 'left':{
+
+            this.x -= spaces;
+            break;
+        }
+        case 'right':{
+            this.x += spaces;
+            break;
+        }
+        case 'up':{
+            this.y -= spaces;
+            break;
+        }
+        case 'down':{
+            this.y += spaces;
+            break;
+        }
+    }
+};
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+var ypositions = [60,140,220];
+var spaces = 95;
+
+var bug1 = new Enemy();
+var bug2 = new Enemy();
+var bug3 = new Enemy();
+var allEnemies = [bug1, bug2, bug3];
+
+var p1 = new Player();
+var player = p1;
 
 
 // This listens for key presses and sends the keys to your
