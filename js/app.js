3d96b356aef2c41a9e8494f202645b08f897a8f7
Alex Erfurt <erfurtalex@gmail.com>
2016-02-28 18:26:53 +0000

    Improved code quality
    Ran JS Hint and JS Beautifier in order to get a better indentation and
    get rid of semicolon mistakes

--- js/app.js
+++ js/app.js
@@ -1,46 +1,36 @@
 // Enemies our player must avoid
-var Enemy = function(x,y) {
+var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-	this.x = x;
-	this.y = y;
-	
+    this.x = x;
+    this.y = y;
+
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
     // Random variables to define speed and starting position of enemyies randomly
-	var random1 = Math.random();
-	var random2 = Math.random();
-	var random3 = Math.random();
-	
-	dx = 500 * dt * random1;
-	if (random2 > 0.9) {
-		dx = dx * 2;
-	}
-	
-	this.x = this.x + dx;
+    var random1 = Math.random();
+    var random2 = Math.random();
+    var yPos = [60, 143, 226, 309, 392];
 
-	if (this.x > 600) {
-		this.x = -100 * random2;
-		if (random3 < 0.16) {
-		this.y = 60;
-		} else if (random3 > 0.16 && random3 < 0.33) {
-		this.y = 143;
-		} else if (random3 > 0.33 && random3 < 0.50) {
-		this.y = 226;
-		} else if (random3 > 0.50 && random3 < 0.67) {
-		this.y = 309;
-		} else {
-		this.y = 392;
-		}
-	}
-};	
+    dx = 500 * dt * random1;
+    if (random2 > 0.9) {
+        dx = dx * 2;
+    }
+    this.x = this.x + dx;
+
+    // Defining starting positions of new enemy
+    if (this.x > 600) {
+        this.x = -101 * random2;
+        this.y = yPos[Math.floor(Math.random() * yPos.length)];
+    }
+};
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
@@ -50,56 +40,56 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function(x,y) {
-	this.sprite = 'images/char-boy.png';
-	this.x = x;
-	this.y = y;
+var Player = function(x, y) {
+    this.sprite = 'images/char-boy.png';
+    this.x = x;
+    this.y = y;
 };
 
 Player.prototype.render = function() {
-   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 Player.prototype.update = function(dt) {
-	maxEnemies = allEnemies.length;
-	var checkCollisionX = 0;
-	var checkCollisionY = 0;
-	for (enemyNumber = 0; enemyNumber < maxEnemies; enemyNumber++) {
-		checkCollisionX = Math.abs(this.x - allEnemies[enemyNumber].x);
-		checkCollisionY = Math.abs(this.y - allEnemies[enemyNumber].y);
-		if (checkCollisionX < 50 && checkCollisionY < 50 || this.y < 0) { //
-			this.x = 202;
-			this.y = 569;
-			//console.log(checkCollisionX,checkCollisionY)
-		}
-	}
+    maxEnemies = allEnemies.length;
+    var checkCollisionX = 0;
+    var checkCollisionY = 0;
+    for (enemyNumber = 0; enemyNumber < maxEnemies; enemyNumber++) {
+        checkCollisionX = Math.abs(this.x - allEnemies[enemyNumber].x);
+        checkCollisionY = Math.abs(this.y - allEnemies[enemyNumber].y);
+        if (checkCollisionX < 50 && checkCollisionY < 50 || this.y < 0) { //
+            this.x = 202;
+            this.y = 569;
+        }
+    }
 };
 
-Player.prototype.handleInput = function(action){
-	var stepX = 101;
-	var stepY = 83;
-	if (action === 'left' && this.x > 0) {
-		this.x = this.x - stepX;
-	} else if (action === 'right' && this.x < 403) {
-		this.x = this.x + stepX;
-	} else if (action === "up") {
-		this.y = this.y - stepY;
-	} else if (action === "down" && this.y < 652) {
-		this.y = this.y + stepY;
-	}
+Player.prototype.handleInput = function(action) {
+    var stepX = 101;
+    var stepY = 83;
+    if (action === 'left' && this.x > 0) {
+        this.x = this.x - stepX;
+    } else if (action === 'right' && this.x < 403) {
+        this.x = this.x + stepX;
+    } else if (action === "up") {
+        this.y = this.y - stepY;
+    } else if (action === "down" && this.y < 569) {
+        this.y = this.y + stepY;
+    }
 };
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var enemy1 = new Enemy(-45,60);
-var enemy2 = new Enemy(300,143);
-var enemy3 = new Enemy(150,226);
-var enemy4 = new Enemy(-45,309);
-var enemy5 = new Enemy(-100,392);
-//var enemy6 = new Enemy(250,475);
-var allEnemies = [enemy1, enemy2, enemy3, enemy4, enemy5];
-var player = new Player(202,569);
+var allEnemies = [];
+var xStartPos = [-45, 300, 150, -45, -100];
+var yStartPos = [60, 143, 226, 309, 392];
+
+for (var i = 0; i < 5; i++) {
+    allEnemies.push(new Enemy(xStartPos[i], yStartPos[i]));
+}
+
+var player = new Player(202, 569);
 
 
 // This listens for key presses and sends the keys to your
@@ -112,7 +102,6 @@ document.addEventListener('keyup', function(e) {
         40: 'down'
     };
 
-    //console.log(allowedKeys[e.keyCode]);
-	player.handleInput(allowedKeys[e.keyCode]);
-	
+    player.handleInput(allowedKeys[e.keyCode]);
+
 });
