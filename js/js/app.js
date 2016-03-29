672527d1ce974fcbe265aa3df58b23bb6fa7386e
vanessa-sangiorgio <vanessa.sangiorgio@yahoo.co.uk>
2016-03-29 19:26:11 +0100

    player enemies and collision

---
+++ js/js/app.js
@@ -0,0 +1,127 @@
+// Enemies our player must avoid
+var Enemy = function(x, y, speed) {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+
+    this.x = x;
+    this.y = y;
+    this.speed = Math.floor(Math.random() * (200)) +100;
+
+    this.sprite = 'images/enemy-bug.png';
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    this.x += this.speed * dt;
+    if (this.x > 505) {
+      this.x= 1;
+    }
+};
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+
+///Player
+// Enemies our player must avoid
+var Player = function(x,y) {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.x = x;
+    this.y = y;
+    this.sprite = 'images/char-boy.png';
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Player.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+
+    this.checkCollisions(dt);
+};
+
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+///END Player
+
+// Draw the enemy on the screen, required method for game
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+
+var allEnemies= function () {
+
+};
+allEnemies.prototype.render= function(){
+
+
+};
+var allEnemies = [new Enemy(300, 55.5), new Enemy(200, 135), new Enemy(300, 225), new Enemy(-400, 135)];
+// Place the player object in a variable called player
+
+var player = new Player(203, 400);
+
+Player.prototype.handleInput = function(direction) {
+  if(direction == "left" && this.x > 25){
+    this.x -= 100;
+  }
+  else if(direction == "right" && this.x < 400){
+    this.x += 100;
+  }
+  else if(direction == "up" && this.y > 0){
+    this.y -= 82.5;
+  }
+  else if(direction == "down" && this.y < 400){
+    this.y += 82.5;
+  }
+};
+
+Player.prototype.reset = function() {
+  this.x = 200;
+  this.y = 400;
+};
+
+Player.prototype.checkCollisions = function() {
+  for (var i=0; i < allEnemies.length; i++){
+    var enemy = allEnemies[i];
+    if (this.x >= enemy.x + 0 &&
+        this.x < enemy.x + 50 &&
+        this.y >= enemy.y + 0 &&
+        this.y < enemy.y + 50)
+{
+  this.reset();
+}
+  }
+};
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
---
+++ js/js/resources.js
@@ -0,0 +1,111 @@
+/* Resources.js
+ * This is simply an image loading utility. It eases the process of loading
+ * image files so that they can be used within your game. It also includes
+ * a simple "caching" layer so it will reuse cached images if you attempt
+ * to load the same image multiple times.
+ */
+(function() {
+    var resourceCache = {};
+    var loading = [];
+    var readyCallbacks = [];
+
+    /* This is the publicly accessible image loading function. It accepts
+     * an array of strings pointing to image files or a string for a single
+     * image. It will then call our private image loading function accordingly.
+     */
+    function load(urlOrArr) {
+        if(urlOrArr instanceof Array) {
+            /* If the developer passed in an array of images
+             * loop through each value and call our image
+             * loader on that image file
+             */
+            urlOrArr.forEach(function(url) {
+                _load(url);
+            });
+        } else {
+            /* The developer did not pass an array to this function,
+             * assume the value is a string and call our image loader
+             * directly.
+             */
+            _load(urlOrArr);
+        }
+    }
+
+    /* This is our private image loader function, it is
+     * called by the public image loader function.
+     */
+    function _load(url) {
+        if(resourceCache[url]) {
+            /* If this URL has been previously loaded it will exist within
+             * our resourceCache array. Just return that image rather
+             * re-loading the image.
+             */
+            return resourceCache[url];
+        } else {
+            /* This URL has not been previously loaded and is not present
+             * within our cache; we'll need to load this image.
+             */
+            var img = new Image();
+            img.onload = function() {
+                /* Once our image has properly loaded, add it to our cache
+                 * so that we can simply return this image if the developer
+                 * attempts to load this file in the future.
+                 */
+                resourceCache[url] = img;
+
+                /* Once the image is actually loaded and properly cached,
+                 * call all of the onReady() callbacks we have defined.
+                 */
+                if(isReady()) {
+                    readyCallbacks.forEach(function(func) { func(); });
+                }
+            };
+
+            /* Set the initial cache value to false, this will change when
+             * the image's onload event handler is called. Finally, point
+             * the image's src attribute to the passed in URL.
+             */
+            resourceCache[url] = false;
+            img.src = url;
+        }
+    }
+
+    /* This is used by developers to grab references to images they know
+     * have been previously loaded. If an image is cached, this functions
+     * the same as calling load() on that URL.
+     */
+    function get(url) {
+        return resourceCache[url];
+    }
+
+    /* This function determines if all of the images that have been requested
+     * for loading have in fact been properly loaded.
+     */
+    function isReady() {
+        var ready = true;
+        for(var k in resourceCache) {
+            if(resourceCache.hasOwnProperty(k) &&
+               !resourceCache[k]) {
+                ready = false;
+            }
+        }
+        return ready;
+    }
+
+    /* This function will add a function to the callback stack that is called
+     * when all requested images are properly loaded.
+     */
+    function onReady(func) {
+        readyCallbacks.push(func);
+    }
+
+    /* This object defines the publicly accessible functions available to
+     * developers by creating a global Resources object.
+     */
+    window.Resources = {
+        load: load,
+        get: get,
+        onReady: onReady,
+        isReady: isReady
+    };
+})();
