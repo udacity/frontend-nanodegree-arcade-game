06817f3af3f016db4a6df864e346a03e16272c30
paulofreitasnobrega <paulofreitasnobrega@gmail.com>
2016-07-22 09:29:10 -0300

    Include Engine Object

--- js/app.js
+++ js/app.js
@@ -1,70 +1,30 @@
-// Creating Objects
+// Creating objects
 var config = new Config,
-    resources = new Resources,
-    scenario = new Scenario,
-    routes = new Routes,
-    traffic = new Traffic;
+    resources   = new Resources,
+    scenario    = new Scenario,
+    routes      = new Routes,
+    traffic     = new Traffic,
+    engine      = new Engine(this);
 
 // Assigning settings
 resources.setConfig(config.select('resources'));
 scenario.setConfig(config.select('scenario'));
 traffic.setConfig(config.select('traffic'));
 
-// Creating Routes
-routes.create(
-    scenario.rowsForEnvironment(),
-    resources.imageSize('height')
-);
+// Creating routes
+routes.create(scenario.rowsForEnvironment(), resources.imageSize('height'));
 
-// Assigning routes to my traffic manager
+// Assigning routes to traffic manager
 traffic.setRoutes(this.routes);
 
+// Create canvas
+var imgWidth        = this.resources.imageSize('width');
+    imgHeight       = this.resources.imageSize('height'),
+    imgFull         = this.resources.imageSize('full');
+    scenarioWidth   = this.scenario.width(imgWidth);
+    scenarioHeight  = this.scenario.height(imgHeight, imgFull);
 
-/*
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+this.engine.createCanvas(scenarioWidth, scenarioHeight);
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
-
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
-
-    player.handleInput(allowedKeys[e.keyCode]);
-});
-*/
+// Run engine
+this.engine.run();
