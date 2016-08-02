63cd912e9a6b4b3ff46c261d76a9d17d248e7c8b
Yetzirah <yetzirahespejo@gmail.com>
2016-08-02 10:48:19 -0400

    Added new feature for set initial position of objects

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x,y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     //Agregar Comentario
     this.sprite = "images/enemy-bug.png";
-    this.x = 0;
-    this.y = 100;
+    this.x = x;
+    this.y = y;
 };
 
 // Update the enemy's position, required method for game
@@ -28,10 +28,10 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function(){
+var Player = function(x,y){
     this.sprite = "images/char-boy.png";
-    this.x = 200;
-    this.y = 400;
+    this.x = x;
+    this.y = y;
 };
 
 Player.prototype.render = function() {
@@ -45,15 +45,12 @@ Player.prototype.update = function(dt) {
     //this.x *= dt;
 };
 
-
-
-
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player\
 
-var player = new Player();
-var enemy = new Enemy();
+var player = new Player(200,400);
+var enemy = new Enemy(0,0);
 var allEnemies = new Array();
 allEnemies.push(enemy);
 // enemy.update(100);
