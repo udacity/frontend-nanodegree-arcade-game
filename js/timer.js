1cc6002884f83483d5e699a887b17e8e618e5e1f
paulofreitasnobrega <paulofreitasnobrega@gmail.com>
2016-08-03 07:58:24 -0300

    Doc Timer

--- js/timer.js
+++ js/timer.js
@@ -1,13 +1,32 @@
+/**
+ * @description It lets you control some aspects of time.
+ * @constructor
+ */
 var Timer = function() {};
 
+/**
+ * @description Convert seconds to milliseconds.
+ * @param  {number} seconds
+ * @return {number}
+ */
 Timer.prototype.secondsToMilliseconds = function(seconds) {
     return seconds * 1000;
 };
 
+/**
+ * @description Create an instance of Date at a future time.
+ * @param  {number} seconds
+ * @return {object} Date
+ */
 Timer.prototype.createFutureTime = function(seconds) {
     return new Date(Date.now() + this.secondsToMilliseconds(seconds));
 };
 
+/**
+ * @description Compare dates to indicate whether a given time has been reached.
+ * @param  {object} futureTime - Date Instance
+ * @return {boolean}
+ */
 Timer.prototype.isFutureTime = function(futureTime) {
     var now = Date.now();
     return now >= futureTime.getTime() ? true : false;
