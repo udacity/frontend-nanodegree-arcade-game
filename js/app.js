d4136fcec46a363a25d417f49fdf0cde06521756
Heather Nuffer <zzzuzz@gmail.com>
2016-08-16 20:00:15 -0600

    Add code for Enemy and Player classes

--- js/app.js
+++ js/app.js
@@ -6,6 +6,9 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = 0;
+    this.y = Math.floor((Math.random() * 225) + 25);
+    this.vel = Math.floor((Math.random() * 100) + 10);
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +17,10 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = this.x + dt * this.vel;
+    if (this.x > 505) {
+      this.x = -10;
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,13 +31,65 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+  this.sprite = "images/char-boy.png";
+  this.x = 250;
+  this.y = 400;
+};
 
+Player.prototype.update = function(dt) {
+  if (this.x > 420) {
+    this.x = 420;
+  }
+  if (this.x < -15) {
+    this.x = -15;
+  }
+  if (this.y > 400) {
+    this.y = 400;
+  }
+  if (this.y < -10) {
+    this.y = -10;
+  }
+};
+
+Player.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(key) {
+  switch (key) {
+    case 'left':
+      this.x -= 10;
+      break;
+
+    case 'right':
+      this.x += 10;
+      break;
+
+    case 'up':
+      this.y -= 10;
+      break;
+
+    case 'down':
+      this.y += 10;
+      break;
+
+    default:
+      console.log("Illegal keypress");
+  }
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+var enemy1 = new Enemy();
+var enemy2 = new Enemy();
+var enemy3 = new Enemy();
 
+var allEnemies = [enemy1, enemy2, enemy3];
+
+var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
