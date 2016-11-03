a8698a67729fce4567d9ab14da3c579ef14fccd1
David Thorpe <vegas_71@yahoo.co.uk>
2016-11-03 19:58:00 +0000

    Added basic outline for Player class

--- js/app.js
+++ js/app.js
@@ -24,7 +24,23 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
 
+    this.sprite = 'images/char-boy.png';
+};
+
+Player.prototype.update = function(dt) {
+    
+};
+
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function() {
+
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
