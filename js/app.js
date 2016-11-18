fa6ecfb24ab4cf409f7f32856e4acc249d6e5b0c
gel00 <gellaigabor@gmail.com>
2016-11-18 14:36:45 +0100

    working alfa version

--- js/app.js
+++ js/app.js
@@ -1,19 +1,41 @@
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+var Map = {
+    rowHeight: 83,
+    colWidth: 101,
+    offsetY: 41
+};
 // Enemies our player must avoid
 var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.setStartCol();
+    this.generateRow();
+    this.generateSpeed();
 };
 
+Enemy.prototype.generateRow = function() {
+    this.y = getRandomInt(1, 3) * Map.rowHeight - Map.offsetY;
+};
+Enemy.prototype.setStartCol = function() {
+    this.x = 0 - Map.colWidth;
+};
+Enemy.prototype.generateSpeed = function() {
+    this.speed = getRandomInt(1,4);
+};
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    if (this.x >= Map.colWidth*5) {
+            this.generateRow();
+            this.generateSpeed();
+            this.setStartCol();
+        } else {
+            this.x += Map.colWidth * dt * this.speed;
+        }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -21,11 +43,43 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
-
+var Player = function() {
+    this.sprite = 'images/char-horn-girl.png';
+    this.x = Map.colWidth * 2;
+    this.y = Map.rowHeight * 5 - Map.offsetY;
+};
+Player.prototype.reset =function(){
+    this.y = Map.rowHeight * 5 - this.offsetY;
+};
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+Player.prototype.update = function() {
+};
+Player.prototype.handleInput = function(direction) {
+    switch(direction) {
+        case 'left':
+            if (this.x > 0) {           //not on first coll
+                this.x -= Map.colWidth;
+            }
+            break;
+        case 'right':
+            if (this.x < Map.colWidth * 4) {  //not on last col
+                this.x += Map.colWidth;
+            }
+            break;
+        case 'up':
+            if (this.y > Map.rowHeight-this.offsetY ||  //first row
+                this.x === Map.colWidth * 2) {      //under the bridge
+                this.y -= Map.rowHeight;
+            }
+            break;
+        case 'down':
+            if (this.y < Map.rowHeight * 5 - Map.offsetY) {        //last row
+                this.y += Map.rowHeight;
+            }
+        }
+};
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
@@ -44,3 +98,11 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+var player = new Player();
+var allEnemies = [];
+var createEnemies = setInterval(function(){
+    allEnemies.push(new Enemy);
+    if (allEnemies.length > 1 ) {
+        clearInterval(createEnemies);
+    }
+}, 400);
--- js/engine.js
+++ js/engine.js
@@ -171,7 +171,16 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-boy.png'
+        'images/char-boy.png',
+        'images/char-cat-girl.png',
+        'images/char-horn-girl.png',
+        'images/char-pink-girl.png',
+        'images/char-princess-girl.png',
+        'images/Selector.png',
+        'images/Gem Blue.png',
+        'images/Gem Green.png',
+        'images/Gem Orange.png',
+        'images/Key.png'
     ]);
     Resources.onReady(init);
 
