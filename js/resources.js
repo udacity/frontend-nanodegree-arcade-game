6498f1e15b4e52e2f15514158e0315489472aad5
Tyler Saballus <tysabs@gmail.com>
2016-12-10 00:22:01 -0600

    Remove attempts and sound caching

--- js/resources.js
+++ js/resources.js
@@ -6,7 +6,6 @@
  */
 (function() {
     var resourceCache = {};
-    var soundCache = {};
     var loading = [];
     var readyCallbacks = [];
 
@@ -57,7 +56,7 @@
                 /* Once the image is actually loaded and properly cached,
                  * call all of the onReady() callbacks we have defined.
                  */
-                if(isImageReady()) {
+                if(isReady()) {
                     readyCallbacks.forEach(function(func) { func(); });
                 }
             };
@@ -79,87 +78,18 @@
         return resourceCache[url];
     }
 
-    function loadSounds(urlOrArr) {
-        if(urlOrArr instanceof Array) {
-            /* If the developer passed in an array of images
-             * loop through each value and call our image
-             * loader on that image file
-             */
-            urlOrArr.forEach(function(url) {
-                _loadSounds(url);
-            });
-        } else {
-            /* The developer did not pass an array to this function,
-             * assume the value is a string and call our image loader
-             * directly.
-             */
-            _loadSounds(urlOrArr);
-        }
-    }
-
-    function _loadSounds(url) {
-        if(soundCache[url]) {
-            /* If this URL has been previously loaded it will exist within
-             * our soundCache array. Just return that image rather
-             * re-loading the image.
-             */
-            return soundCache[url];
-        } else {
-            /* This URL has not been previously loaded and is not present
-             * within our cache; we'll need to load this image.
-             */
-            var sound = new Audio();
-            sound.onload = function() {
-                /* Once our image has properly loaded, add it to our cache
-                 * so that we can simply return this image if the developer
-                 * attempts to load this file in the future.
-                 */
-                soundCache[url] = sound;
-
-                /* Once the image is actually loaded and properly cached,
-                 * call all of the onReady() callbacks we have defined.
-                 */
-                if(isSoundReady()) {
-                    readyCallbacks.forEach(function(func) { func(); });
-                }
-            };
-
-            /* Set the initial cache value to false, this will change when
-             * the image's onload event handler is called. Finally, point
-             * the image's src attribute to the passed in URL.
-             */
-            soundCache[url] = false;
-            sound.src = url;
-        }
-    }
-
-    function getSounds(url) {
-        return soundCache[url];
-    }
-
     /* This function determines if all of the images that have been requested
      * for loading have in fact been properly loaded.
      */
-    function isImageReady() {
-        var imageReady = true;
-        for(var k in resourceCache ) {
-            if (resourceCache.hasOwnProperty(k) &&
+    function isReady() {
+        var ready = true;
+        for(var k in resourceCache) {
+            if(resourceCache.hasOwnProperty(k) &&
                !resourceCache[k]) {
-                imageReady = false;
+                ready = false;
             }
         }
-        return imageReady;
-    }
-
-    function isSoundReady() {
-        var soundReady = true;
-        for(var k in soundCache ) {
-            if (soundCache.hasOwnProperty(k) &&
-               !soundCache[k]) {
-                soundReady = false;
-            }
-        }
-        return soundReady;
+        return ready;
     }
 
     /* This function will add a function to the callback stack that is called
@@ -174,11 +104,8 @@
      */
     window.Resources = {
         load: load,
-        loadSounds: loadSounds,
         get: get,
-        getSounds: getSounds,
         onReady: onReady,
-        isImageReady: isImageReady,
-        isSoundReady: isSoundReady
+        isReady: isReady
     };
 })();
