2ff0ea7986be7c713ee2a2c01d9ff2a941b707d7
MujunZ <o.6180339@gmail.com>
2016-12-14 00:12:14 -0500

    Show the canvas!!!!! Move player Show enemy Need to figure out how to push a big wave of enemies.

--- js/app.js
+++ js/app.js
@@ -6,6 +6,9 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = 0;
+    this.y = Math.floor(Math.random()*3)*90+50;
+    this.speed = 2;
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +17,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+   this.x += dt*100;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -21,15 +25,57 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function () {
+    this.sprite = "images/char-boy.png";
+    this.x = 200;
+    this.y = 400;
+}
 
+Player.prototype.update = function(dt) {
+    
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(key) {
+    console.log(key);
+    switch (key) {
+        case "right":
+            if (this.x < 350) {
+                this.x += 100;
+            }
+            break;
+        case "left":
+            if (this.x > 50){
+                this.x -= 100;
+            }
+            break;
+        case "up":
+            if (this.y > 0){
+                this.y -= 90;
+            }
+            break;
+        case "down":
+            if (this.y < 400){
+                this.y += 90;
+            }
+            break;
+        default:
+            console.log("no movement");
+    }
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var allEnemies = [new Enemy, new Enemy, new Enemy];
+var player = new Player;
 
 
 // This listens for key presses and sends the keys to your
