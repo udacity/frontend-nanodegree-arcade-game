f7a078a2ea441d9ef2bd11ec32c9e73c28bad302
Tamas Kocsis <kocsis.tamas88@gmail.com>
2016-12-16 22:59:33 +0100

    One enemy appears

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -14,6 +16,11 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.speed * dt;
+
+    if (this.x >= 505) {
+        this.x = 0;
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,13 +31,23 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+// var Player = function(x,y,speed) {
+//     this.x = x;
+//     this.y = y;
+//     this.speed = speed;
+//     this.sprite = 'images/char-princess-girl.png'
+// };
 
+// Player.prototype.render = function {
+//     ctx.drawImage(Resources.get(this.sprite), this.x, this.y;
+// };
 
 // Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+var allEnemies = [];// Place all enemy objects in an array called allEnemies
+// var player = new Player(202.5, 383, 50);// Place the player object in a variable called player
+var enemy = new Enemy(0, Math.random() * 184 + 50, Math.random() * 256); //randomized enemies occur
 
-
+allEnemies.push(enemy);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -42,5 +59,5 @@ document.addEventListener('keyup', function(e) {
         40: 'down'
     };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+//    player.handleInput(allowedKeys[e.keyCode]);
 });
