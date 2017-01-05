ee9ec0c3e9f188ac5b607a42660833433ecf4740
DOEHOONLEE <DavidL@DOEs-MacBook.local>
2017-01-05 17:58:35 +0900

    update - 1.0
    - player and game board visible
    - player movable

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
+/*       ENEMY       */
+
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x,y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+    this.x = x;
+    this.y = y;
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+    this.sprite = "images/enemy-bug.png";
 };
 
 // Update the enemy's position, required method for game
@@ -21,16 +25,48 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+/*         PLAYER         */
+
 // Now write your own player class
+var Player = function(x,y) {
+    this.x = x;
+    this.y = y;
+    
+    this.sprite = "images/char-boy.png"
+};
+
 // This class requires an update(), render() and
 // a handleInput() method.
+Player.prototype.update = function(dt) {
+    
+};
 
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(inputKey) {
+    if (inputKey == 'up') {
+        player.y = player.y - 50;
+    }
+    if (inputKey == 'right') {
+        player.x = player.x + 50;
+    }
+    if (inputKey == 'down') {
+        player.y = player.y + 50;
+    }
+    if (inputKey == 'left') {
+        player.x = player.x - 50;
+    }
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+var player = new Player(200,400);
 
+var allEnemies = [];
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
