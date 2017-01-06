41fbef188cfaae12f01f2b4caadd0ccf124a5437
DOEHOONLEE <DavidL@DOEs-MacBook.local>
2017-01-06 12:34:03 +0900

    update - 2.0
    - player movement is now restricted within the game board
    - gems added
    - 4 bugs moving

--- js/engine.js
+++ js/engine.js
@@ -94,6 +94,7 @@ var Engine = (function(global) {
         allEnemies.forEach(function(enemy) {
             enemy.update(dt);
         });
+        
         player.update();
     }
 
@@ -150,10 +151,15 @@ var Engine = (function(global) {
         allEnemies.forEach(function(enemy) {
             enemy.render();
         });
-
+        
+        allGems.forEach(function(gem) {
+            gem.render();
+        });
+        
         player.render();
+    
     }
-
+    
     /* This function does nothing but it could have been a good place to
      * handle game reset states - maybe a new game menu or a game over screen
      * those sorts of things. It's only called once by the init() method.
@@ -171,7 +177,7 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-boy.png'
+        'images/char-boy.png', 'images/Gem-Green.png', 'images/Key.png'
     ]);
     Resources.onReady(init);
 
