987afb56dbc866040409828c3c89081adb58d9a7
pllearns@gmail.com <philliplorenzo@Phillips-MacBook-Pro.local>
2017-02-27 07:57:22 -0800

    slight refactor, cleaned up comments

--- js/app.js
+++ js/app.js
@@ -1,39 +1,22 @@
-// Enemies our player must avoid
 var Enemy = function(x, y, speed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
     this.x = x;
     this.y = y;
     this.speed = speed;
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
     this.x += this.speed * dt;
 
     if(this.x >= 505) {
       this.x = 0;
     }
-
     checkCollision(this)
 };
 
-// Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
 
 var Player = function(x, y, speed) {
   this.x = x;
@@ -51,24 +34,17 @@ Player.prototype.render = function() {
 };
 
 var checkCollision = function(anEnemy) {
-    // check for collision between enemy and player
-    if (
-        player.y + 131 >= anEnemy.y + 90
+    if (player.y + 131 >= anEnemy.y + 90
         && player.x + 25 <= anEnemy.x + 88
         && player.y + 73 <= anEnemy.y + 135
         && player.x + 76 >= anEnemy.x + 11) {
-        console.log('collided');
-        player.x = 202.5;
-        player.y = 383;
-    }
-
-    // check for player reaching top of canvas and winning the game
-    // if player wins, add 1 to the score and level
-    // pass score as an argument to the increaseDifficulty function
+          console.log('collided');
+          player.x = 202.5;
+          player.y = 383;
+      }
     if (player.y + 63 <= 0) {
         player.x = 202.5;
         player.y = 383;
-        console.log('you made it!');
 
         ctx.fillStyle = 'white';
         ctx.fillRect(0, 0, 505, 171);
@@ -77,11 +53,8 @@ var checkCollision = function(anEnemy) {
         gameLevel += 1;
         console.log('current score: ' + score + ', current level: ' + gameLevel);
         increaseDifficulty(score);
+      }
 
-    }
-
-    // check if player runs into left, bottom, or right canvas walls
-    // prevent player from moving beyond canvas wall boundaries
     if (player.y > 383 ) {
         player.y = 383;
     }
@@ -92,7 +65,6 @@ var checkCollision = function(anEnemy) {
         player.x = 2.5;
     }
 };
-
 
 Player.prototype.handleInput = function(onPress) {
   if(onPress === 'left') {
@@ -109,18 +81,12 @@ Player.prototype.handleInput = function(onPress) {
   }
 }
 
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
 var allEnemies = [];
 var player = new Player();
 
 var enemy = new Enemy(0,Math.random() * 184 + 50, Math.random() * 256);
 allEnemies.push(enemy);
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
