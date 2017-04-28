6cb0bb5c17ad6f00aedf1646ff58bac42bdaf580
Robert Naccache <RNaccache@ccc.net>
2017-04-28 09:54:35 +0300

    Player and Enemies rendered

--- js/app.js
+++ js/app.js
@@ -6,6 +6,9 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = 1;
+    this.y = 60;
+    this.speed = 1;
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +17,9 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    console.log(this.x);
+    this.x = this.x * dt + 50;
+    console.log(dt);
 };
 
 // Draw the enemy on the screen, required method for game
@@ -25,12 +31,49 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+//202 360
+
+var Player = function(){
+    this.sprite = 'images/char-boy.png';
+    this.x = 202;
+    this.y = 392;
+
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.update = function(x,y) {
+    if (x != undefined && y != undefined){
+        this.x += x;
+        this.y += y;
+    }
+}
+;
+
+Player.prototype.handleInput = function (keyCode){
+    if(keyCode === "left"){
+        this.update(-101, 0);
+    }
+    else if(keyCode === "up"){
+        this.update(0, -83);
+    }
+    else if(keyCode === "down"){
+        this.update(0, 83);
+    }
+    else if(keyCode === "right"){
+        this.update(101, 0);
+    }
+};
+
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-
+var player = new Player();
+var allEnemies = [new Enemy()];
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
