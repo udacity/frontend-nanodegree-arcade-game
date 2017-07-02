c27734e9f43065c112b7208944d47efd4f8d62d2
Mariamma Antony <Mariamma.Antony@aexp.com>
2017-07-02 21:59:47 +0530

    adding player and enemy class

--- js/app.js
+++ js/app.js
@@ -25,12 +25,28 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var Player = function(){
+    this.sprite = 'images/char-cat-girl.png';
+};
+
+Player.prototype.update = function(dt){
+};
+
+Player.prototype.render = function(){
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+var enemy1 = new Enemy();
+var enemy2 = new Enemy();
+var enemy3 = new Enemy();
+var allEnemies = [enemy1, enemy2, enemy3];
 
+var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
