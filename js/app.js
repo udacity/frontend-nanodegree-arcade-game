91b8e4713094c06adb1d466a4272d69f682872ea
haifaalmndeel <haifaa.almndeel@gmail.com>
2017-09-22 00:51:30 +0300

    V1

--- js/app.js
+++ js/app.js
@@ -1,11 +1,17 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(speed, row) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.speed = speed;
+    this.row=row;
+    this.x=0;
+    this.y=0;
+
+
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +20,12 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x=this.x+this.speed;
+    if (this.x<=player.x && this.x>=player.x+83 && this.y==player.y) {
+        //function reset
+       player.reset();
+    }
+
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,6 +36,42 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+    var player = function() {
+ 
+    this.sprite = 'char-princess-girl';
+    this.x=249;
+    this.y=506;
+
+    this.reset= function(){
+    this.x=249;
+    this.y=506;
+    }
+
+   this.handleInput =function(key){
+    if (key== "left" && this.x!=0) {
+        this.x=this.x-83;
+        if (true) {}
+    }
+    else if (key== "right"&& this.x<=332) {
+        this.x=this.x+83;
+    }
+    /////
+   }
+
+};
+   
+   player.prototype.update = function(dt) {
+    
+   }
+
+   player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+   
+ var player=new player();
+ 
+ var Enemy =new Enemy(20,1);
+
 
 
 // Now instantiate your objects.
