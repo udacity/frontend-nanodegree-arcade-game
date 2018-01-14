ff8408049efb5103ffae45d7f53f7881dd327e1d
Business-Art-Technology-MAN <joefficient@gmail.com>
2018-01-13 21:11:39 -0600

    basic functionality implemented

--- js/app.js
+++ js/app.js
@@ -1,3 +1,26 @@
+
+//Base Class for all Entities in game
+class gameEntity{
+    constructor(mySpriteImgPath, myX, myY){
+        this.sprite = mySpriteImgPath;
+        this.x = myX;
+        this.y = myY;
+    }
+    render(){
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+}
+// Enemies our player must avoid
+class Enemy extends gameEntity{
+    constructor(mySpriteImgPath, myX, myY){
+        super(mySpriteImgPath, myX, myY);
+    }
+    update(dt){
+        this.x = this.x * (1+dt);
+
+    }
+}
+/* Original Enemies code
 // Enemies our player must avoid
 var Enemy = function() {
     // Variables applied to each of our instances go here,
@@ -20,17 +43,34 @@ Enemy.prototype.update = function(dt) {
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
-
+*/
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+class playerClass extends gameEntity{
+    constructor(mySpriteImgPath, myX, myY){
+        super(mySpriteImgPath, myX, myY);
+    }
+    update(dt){
 
+    }
+    handleInput(key){
+        if(key === 'left')
+            this.x -= 25;
+        else if(key === 'right')
+            this.x += 25;
+        else if(key === 'up')
+            this.y -= 25;
+        else if(key === 'down')
+            this.y += 25;
+    }
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
-
+var allEnemies = [new Enemy('images/enemy-bug.png', 50, 150), new Enemy('images/enemy-bug.png', 1, 100)];
+var player = new playerClass('images/char-boy.png', 200, 400);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
