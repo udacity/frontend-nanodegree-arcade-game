d3a792cc1adead58dff5d036bdbb31002d4b9325
Fatima Falath <FatimaFalath@github.com>
2018-01-17 21:23:13 +0300

    my implementation
    ?

--- README.md
+++ README.md
@@ -1,6 +1,23 @@
 frontend-nanodegree-arcade-game
 ===============================
+#Description
+This is my implementation of the classic arcade game Frogger, made for the Udacity Front-End Web Developer Nanodegree.
 
-Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
+#Installation
+To run the game:
+1. Download the zip file and open index.html
+2. Play it here
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+#Goals of the game
+Get the chicken to the other side of the road to get the joke answer!
+(You won’t get the same one every time!)
+
+#Controls
+Use the up, down, right, left keys on the keyboard.
+
+#Resources
+-Original course project files
+-Chicken picture
+-Car picture
+
+##Enjoy!
--- js/app.js
+++ js/app.js
@@ -1,46 +1,101 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+var Enemy = function(row, speed) {
+	// Variables applied to each of our instances go here,
+	// we've provided one for you to get started
+	this.x = -170;
+	this.y = 55 + (80 * row);
+	this.speed = speed;
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+	// The image/sprite for our enemies, this uses
+	// a helper we've provided to easily load images
+	this.sprite = 'images/car-enemy.gif';
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+	// You should multiply any movement by the dt parameter
+	// which will ensure the game runs at the same speed for
+	// all computers.
+	this.x = this.x + this.speed * dt;
+	if (this.x > 500)
+		this.x = -100;
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var Player = function() {
+	this.x = 210;
+	this.y = 440;
+	this.score = 0;
+
+	this.sprite = 'images/chicken.png';
+};
+
+Player.prototype.handleInput = function(key) {
+
+	if (key === 'left') {
+		if (this.x > 100) {
+			this.x = this.x - 100;
+		}
+	} else if (key === 'right') {
+		if (this.x < 400) {
+			this.x = this.x + 100;
+		}
+	} else if (key === 'up') {
+		if (this.y > 0) {
+			this.y = this.y - 80;
+		}
+	} else if (key === 'down') {
+		if (this.y < 380) {
+			this.y = this.y + 80;
+		}
+	}
+};
+
+Player.prototype.reset = function() {
+	this.x = 210;
+	this.y = 440;
+};
+
+Player.prototype.render = function() {
+	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+var allEnemies = [];
 
+for (var s = 1; s < 6; s++) {
+	var randomSpeed = randomNumber(5, 25) * 10;
+	var randomRow = randomNumber(1, 4);
+	allEnemies[s] = new Enemy(randomRow, randomSpeed);
+}
+
+var player = new Player();
+
+function randomNumber(min, max) {
+	return Math.floor(Math.random() * (max - min)) + min;
+}
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+	var allowedKeys = {
+		37: 'left',
+		38: 'up',
+		39: 'right',
+		40: 'down'
+	};
 
-    player.handleInput(allowedKeys[e.keyCode]);
+	player.handleInput(allowedKeys[e.keyCode]);
 });
