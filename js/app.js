36cc0f9fc307f8ece41afcc1836d4602b5ea3c35
Mehmet Ali Ayan <maliayan34@gmail.com>
2018-04-26 14:27:39 +0300

    Adding var Enemy attributes

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, ,y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.x = posX;
+    this.y = posY;
+    this.speed = speedEnemy;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
