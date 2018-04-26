1c6079044304c1bd2b81dfb1403b395fb3087028
Mehmet Ali Ayan <maliayan34@gmail.com>
2018-04-26 14:32:34 +0300

    Adding var Player

--- js/app.js
+++ js/app.js
@@ -31,7 +31,11 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-
+var Player = function() {
+  this.x = 101;
+  this.y = 101;
+  this.sprite = 'images/char-boy.png';
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
