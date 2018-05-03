da610917917e3248bc55c40726847a6f98e9cae7
Chris Camp <cpcamp2@gmail.com>
2018-05-03 13:33:43 -0400

    create Player class, Player update method (empty), Player render method, Player handle input method (empty), array for all enemies and initialized player object.

--- js/app.js
+++ js/app.js
@@ -24,11 +24,29 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(x, y, speed) {
+  this.x = x;
+  this.y = y;
+  this.speed = speed;
+  this.sprite = 'images/char-cat-girl.png';
+};
+
+Player.prototype.update = function() {
+
+};
+
+Player.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function()
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var allEnemies = [];
+var player = new Player();
 
 
 
