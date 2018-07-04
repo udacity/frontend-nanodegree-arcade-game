3c0f2913bcfc5c781682718b6fe2bc7130004659
jdmedlock <jdmedlock@gmail.com>
2018-07-04 10:16:25 -0500

    feature: Add <script> tag to bundle
    Add a <script> tag pointing to the bundle generated by Webpack
    Create a skeletal Player class definition
    
    Resolves: N/a
    See also: N/a

--- js/app.js
+++ js/app.js
@@ -24,12 +24,20 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+// Enemies our player must avoid
+var Player = function() {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
 
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/char-boy.png';
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+let player = new Player();
 
 
 // This listens for key presses and sends the keys to your
