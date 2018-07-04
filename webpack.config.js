e73244209d367f711763863436567f6ba4999ed2
jdmedlock <jdmedlock@gmail.com>
2018-07-04 07:58:12 -0500

    feature: Setup project
    Setup the project with the following components:
    - package.json containing build script and dependencies
    - Add Babel and Webpack dependencies
    - Add .gitignore to ensure components such as Node packages aren't uploaded to the repo
    
    Resolves: N/a
    See also: N/a

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  mode: "production",
+  entry: {
+    app: ["./js/app.js"]
+  },
+  output: {
+    filename: 'bundle-app.js'
+  },
+  module: {
+    rules: [
+        {
+            test: /\.js$/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['es2015']
+            }
+        }
+    ]
+  }
+};
