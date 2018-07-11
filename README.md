99feac27a15d5e473f37b18d4ec41614b67fbdcd
Fernanda Moya <fernandamoya@Fernandas-MacBook-Pro.local>
2018-07-11 17:23:40 -0300

    Translation for code comments and readme file

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 frontend-nanodegree-arcade-game
 ===============================
 
-Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
+Os alunos devem usar esta [rubrica](https://review.udacity.com/#!/rubrics/436/view) para fazer uma autoavaliação de seu projeto. Certifique-se de que as funções que escreveu sejam **orientadas a objetos** - tanto funções de classe (como Player e Enemy) como funções de protótipo de classe, como Enemy.prototype.checkCollisions, e que a palavra-chave 'this' seja usada de maneira correta em suas funções de classe e de protótipo de classe para se referir ao objeto que faz a chamada da função. Certifique-se, também, de que o arquivo **readme.md** esteja atualizado com suas instruções, tanto sobre rodar quanto sobre como jogar o fliperama clássico.
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+Para ver instruções detalhadas sobre como começar, confira este [guia](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
--- js/resources.js
+++ js/resources.js
@@ -1,85 +1,95 @@
 /* Resources.js
- * This is simply an image loading utility. It eases the process of loading
- * image files so that they can be used within your game. It also includes
- * a simple "caching" layer so it will reuse cached images if you attempt
- * to load the same image multiple times.
+ * Esta é apenas uma funcionalidade de carregamento de imagens.
+ * Facilita o processo de carregamento de imagens para que elas
+ * possam ser usadas em seu jogo. Também inclui uma camada
+ * simples de "caching", para que imagens cujo "cache" foi 
+ * feito sejam reutilizadas caso você tente carregar a mesma
+ * imagem várias vezes.
+ * 
  */
 (function() {
     var resourceCache = {};
     var loading = [];
     var readyCallbacks = [];
 
-    /* This is the publicly accessible image loading function. It accepts
-     * an array of strings pointing to image files or a string for a single
-     * image. It will then call our private image loading function accordingly.
+    /* Esta é a função de carregamento de imagens disponível
+     * publicamente. Ela aceita uma array de strings que leva
+     * a arquivos de imagem ou uma string de uma só imagem.
+     * Depois, chama nossa função privada de carregamento 
+     * de imagens de maneira adequada.
      */
     function load(urlOrArr) {
         if(urlOrArr instanceof Array) {
-            /* If the developer passed in an array of images
-             * loop through each value and call our image
-             * loader on that image file
+            /* Se o desenvolvedor passar uma array de imagens,
+             * faça o loop por cada valor e chame nosso
+             * carregador de imagens no arquivo de imagem.
              */
             urlOrArr.forEach(function(url) {
                 _load(url);
             });
         } else {
-            /* The developer did not pass an array to this function,
-             * assume the value is a string and call our image loader
-             * directly.
+            /* O desenvolvedor não passou uma array para esta
+             * função; suponha que o valor seja uma string
+             * e chame nosso carregador de imagens diretamente.
              */
             _load(urlOrArr);
         }
     }
 
-    /* This is our private image loader function, it is
-     * called by the public image loader function.
+    /* Esta é a função do carregador de imagens privado, que
+     * é chamado pela função do carregador de imagens público.
      */
     function _load(url) {
         if(resourceCache[url]) {
-            /* If this URL has been previously loaded it will exist within
-             * our resourceCache array. Just return that image rather
-             * re-loading the image.
+            /* Se esta URL já tiver sido carregada, ela
+             * existirá em nossa array resourceCache. Basta
+             * retornar aquela imagem em vez de recarregá-la.
              */
             return resourceCache[url];
         } else {
-            /* This URL has not been previously loaded and is not present
-             * within our cache; we'll need to load this image.
+            /* Esta URL não foi carregada anteriormente e não
+             * está em nosso cache; precisaremos carregá-la.
              */
             var img = new Image();
             img.onload = function() {
-                /* Once our image has properly loaded, add it to our cache
-                 * so that we can simply return this image if the developer
-                 * attempts to load this file in the future.
+                /* Depois que a imagem tiver sido carregada,
+                 * adicione-a ao cache para que possamos apenas
+                 * retorná-la caso o desenvolvedor tente
+                 * carregar o arquivo novamente no futuro.
                  */
                 resourceCache[url] = img;
 
-                /* Once the image is actually loaded and properly cached,
-                 * call all of the onReady() callbacks we have defined.
+                /* Depois que a imagem tiver mesmo sido
+                 * carregada e seu cache tiver sido feito,
+                 * chame todos os callbacks de onReady()
+                 * que definimos.
                  */
                 if(isReady()) {
                     readyCallbacks.forEach(function(func) { func(); });
                 }
             };
 
-            /* Set the initial cache value to false, this will change when
-             * the image's onload event handler is called. Finally, point
-             * the image's src attribute to the passed in URL.
+            /* Define o valor inicial do cache como false, isso
+             * mudará quando o manipulador do evento onload da
+             * imagem for chamado. Por fim, direcione o atributo
+             * src da imagem para a URL definida.
              */
             resourceCache[url] = false;
             img.src = url;
         }
     }
 
-    /* This is used by developers to grab references to images they know
-     * have been previously loaded. If an image is cached, this functions
-     * the same as calling load() on that URL.
+    /* Isto é usado por desenvolvedores para pegar referências
+     * de imagens que eles sabem que já foram carregadas. Se o 
+     * cache de uma imagem é feito, isso tem o mesmo efeito
+     * que chamar load() naquela URL.
      */
     function get(url) {
         return resourceCache[url];
     }
 
-    /* This function determines if all of the images that have been requested
-     * for loading have in fact been properly loaded.
+    /* Esta função determina se todas as imagens solicitadas
+     * para carregamento foram carregadas corretamente.
      */
     function isReady() {
         var ready = true;
@@ -92,15 +102,17 @@
         return ready;
     }
 
-    /* This function will add a function to the callback stack that is called
-     * when all requested images are properly loaded.
+    /* Essa função adicionará uma função ao stack de
+     * callback que é chamado quando todas as imagens
+     * solicitadas forem corretamente carregadas.
      */
     function onReady(func) {
         readyCallbacks.push(func);
     }
 
-    /* This object defines the publicly accessible functions available to
-     * developers by creating a global Resources object.
+    /* Este objeto define as funções acessíveis
+     * publicamente aos desenvolvedores ao criar
+     * um ojeto de Recursos global.
      */
     window.Resources = {
         load: load,
