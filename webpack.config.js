b6930207f64bb3d9676c46869619d7616d3e3147
Josep <coding1992@outlook.com>
2018-08-18 14:53:19 -0400

    Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,25 @@
 const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-  //
+  entry: ['babel-polyfill', './js/app.js'],
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'build.js',
+  },
+  optimization: {
+    minimizer: [new UglifyJsPlugin()],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          preset: ['env'],
+        },
+      },
+    ],
+  },
 };
