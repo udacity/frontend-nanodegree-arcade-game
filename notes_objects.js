2f571bc73d4e9ae821c45c0dd90dde9e93b9b54b
Mario Burgos <mario.burgos@jpl.nasa.gov>
2018-08-28 16:51:05 -0700

    Updates comments.

--- notes_objects.js
+++ notes_objects.js
@@ -1,12 +1,12 @@
 let string = 'orange'; // Immutable primative
 
 function changeToApple(string) {
-  string = 'apple';
+  string = 'apple'; // Only in function scope
 }
 
 changeToApple(string);
 
-console.log(string);
+console.log(string); // Variable has not been updated
 
 // Instantiate object from exsiting object (pass object by reference)
 const oven = {
