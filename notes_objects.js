63c0b256d06563625f03ee31fb4383f140615c94
CrustyBarnacle <CrustyBarnaclee@users.noreply.github.com>
2018-08-28 21:51:12 -0700

    Adds changes to notes_objects.js

--- notes_objects.js
+++ notes_objects.js
@@ -31,12 +31,19 @@ console.log(oven.temperature); // Original object is updated
 const developer = {
   name: 'Andrew',
   sayHello: function () {
-    console.log('Hi there, ' + this.name + '!');
+    console.log('Hi there, ' + this.name + '!'); // Using this keyword
   }
 };
 
+const bell = {
+  color: 'gold',
+  ring: function () {
+    console.log('Ring ring ring!');
+  }
+};
 
 developer.sayHello();
+bell.ring();
 
 // Passing arguments into Methods
 
@@ -50,8 +57,25 @@ const another_developer = {
   }
 };
 
-
 another_developer.favoriteLanguage('JavaScript');
 
+
+// Calling a method inside an array
 const myArray = [ function alerter() { console.log('Hello!'); } ]; // alert('Hello!'); did NOT work in Atom console :-/
+
 myArray[0]();
+
+
+// Using a method to change the object
+const tree = {
+  type: 'redwood',
+  leaves: 'green',
+  height: 80,
+  age: 15,
+  growOneFoot: function () {
+    this.height += 1;
+  }
+};
+
+tree.growOneFoot();
+console.log(tree.height);
