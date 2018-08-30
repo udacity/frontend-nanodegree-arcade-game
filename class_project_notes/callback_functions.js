d0e54a47b6c4bc08206e1d3c464c28baf8d35756
Mario Burgos <mario.burgos@jpl.nasa.gov>
2018-08-30 16:51:34 -0700

    Adds functions.js and callback_functions.js

---
+++ class_project_notes/callback_functions.js
@@ -0,0 +1,3 @@
+// A function that takes other functions as arguments (and/or returns a function,
+// as we learned in the previous section) is known as a higher-order function.
+// A function that is passed as an argument into another function is called a callback function.
