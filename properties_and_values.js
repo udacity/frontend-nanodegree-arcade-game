b6849374aeee1b7eb3bbacc1bb3e957e04a1e95c
Mario Burgos <mario.burgos@jpl.nasa.gov>
2018-08-30 12:57:00 -0700

    Adds properties_and_values.js

---
+++ properties_and_values.js
@@ -0,0 +1,50 @@
+const myNewFancyObject = new Object();
+
+    Object.keys(myNewFancyObject); // empty as object has no keys
+    Object.values(myNewFancyObject); // empty as object has no keys or values
+
+const dictionary = {
+  car: 'automobile',
+  apple: 'healthy snack',
+  cat: 'cute furry animal',
+  dog: 'best friend',
+  mouse: 2
+};
+
+let dictionary_values = Object.values(dictionary); // new array of Oject.values
+
+const result = [];
+for (const name in dictionary) {
+    result.push(name)
+}
+
+console.log(dictionary_values);
+console.log(result);
+
+const triangle = {
+  type: 'polygon',
+  sides: 3,
+  sumOfAngles: 180,
+  equilateral: true,
+  equiangular: true
+};
+
+let triangle_keys = Object.keys(triangle);
+console.log(triangle_keys);
+
+/*
+*
+*    Object.keys() returns an array of a given object's own keys (property names).
+*    Object.values() returns an array of a given object's own values (property values).
+*
+*
+*    Object.keys() on MDN
+*    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
+*
+*    Object.values() on MDN
+*    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/values
+*
+*    Browser Compatibility
+*    http://tokenposts.blogspot.com.au/2012/04/javascript-objectkeys-browser.html
+*
+*/
