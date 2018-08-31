cd73400ec2b8fcc6d48781dce75287ec9aea8eb4
Mario Burgos <mario.burgos@jpl.nasa.gov>
2018-08-31 14:04:28 -0700

    Updates callback_functions.js

--- class_project_notes/callback_functions.js
+++ class_project_notes/callback_functions.js
@@ -15,7 +15,29 @@ function isPositive(n) {
   return n > 0;
 };
 
+console.log('Positive Numbers: ');
 each([-2, 7, 11, -4, -10], isPositive); // Output: 7, 11
 
 
-// Array Methods
+// Array Methods - function passed in forEach() loops
+console.log('Odd Numbers:');
+[1, 5, 2, 4, 6, 3].forEach(function (n) { // anonymous function
+  if (n % 2 !== 0) {
+    console.log(n);
+  }
+});
+
+[1, 5, 2, 4, 6, 3].forEach(function logIfOdd(n) { // named function
+  if (n % 2 !== 0) {
+    console.log(n);
+  }
+});
+
+function ifOdd(n) {
+    if (n % 2 !== 0) {
+        console.log(n);
+    }
+};
+
+console.log('More odd numbers...'); // previously defined function passed in forEach()
+[1, 3, 5, 4, 6, 2, 0].forEach(ifOdd);
