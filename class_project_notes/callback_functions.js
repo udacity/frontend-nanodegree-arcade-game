2d2edc37edcf619eb8dd6f5012b0b45e80103cd2
Mario Burgos <mario.burgos@jpl.nasa.gov>
2018-09-05 13:05:40 -0700

    Adds map() callback example.

--- class_project_notes/callback_functions.js
+++ class_project_notes/callback_functions.js
@@ -50,3 +50,37 @@ const nameLengths = names.map(function(name) {
 });
 
 console.log(nameLengths);
+
+// From Callbacks quiz
+/* Using map()
+ *
+ * Using the musicData array and map():
+ *   - Return a string for each item in the array in the following format:
+ *     <album-name> by <artist> sold <sales> copies
+ *   - Store the returned data in a new albumSalesStrings variable
+ *
+ * Note:
+ *   - Do not delete the musicData variable
+ *   - Do not alter any of the musicData content
+ *   - Do not format the sales number; leave it as a long string of digits
+ */
+
+const musicData = [
+    { artist: 'Adele', name: '25', sales: 1731000 },
+    { artist: 'Drake', name: 'Views', sales: 1608000 },
+    { artist: 'Beyonce', name: 'Lemonade', sales: 1554000 },
+    { artist: 'Chris Stapleton', name: 'Traveller', sales: 1085000 },
+    { artist: 'Pentatonix', name: 'A Pentatonix Christmas', sales: 904000 },
+    { artist: 'Original Broadway Cast Recording',
+      name: 'Hamilton: An American Musical', sales: 820000 },
+    { artist: 'Twenty One Pilots', name: 'Blurryface', sales: 738000 },
+    { artist: 'Prince', name: 'The Very Best of Prince', sales: 668000 },
+    { artist: 'Rihanna', name: 'Anti', sales: 603000 },
+    { artist: 'Justin Bieber', name: 'Purpose', sales: 554000 }
+];
+
+const albumSalesStrings = musicData.map(function(album) {
+    return album.name + " by " + album.artist + " sold " + album.sales + " copies";
+});
+
+console.log(albumSalesStrings);
