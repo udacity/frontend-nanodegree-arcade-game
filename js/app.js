cb9aad846e92d514a0a86a11f8b36e2f6e7f94a6
Mario Burgos <mario.burgos@jpl.nasa.gov>
2018-09-17 11:03:59 -0700

    Cleaned up comments in app.js

--- js/app.js
+++ js/app.js
@@ -17,14 +17,13 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    
-    // If enemy is not past boundary
+
+    // If enemy is not past boundary, then keep moving
     if(this.x < this.boundary) { // places bug just off screen
         // Move forward
         // Increment x by speed * dt
         this.x += this.speed * dt;
     }
-    // else
     else {
         // Reset position to start
         this.x = this.resetPosition;
@@ -36,9 +35,9 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
+// Player class. This class requires an update(), render() and
 // a handleInput() method.
+// Added resetHero().
 
 class Hero {
     constructor() {
@@ -51,7 +50,7 @@ class Hero {
         this.y = this.startY;
         this.playerWin = false;
     }
-    
+
     updatePosition() {
         // check Collision
         let playerRight = this.x + this.step / 2;
@@ -66,14 +65,14 @@ class Hero {
         if (this.y === 55) {
             this.playerWin = true;
         }
-        
+
     }
-    
+
     // Draw player sprite at current x,y
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
-    
+
     /** Update player x,y based on input
     *
     * @param {string} input - Direction of travel
@@ -102,7 +101,7 @@ class Hero {
                 break;
         }
     }
-    
+
     resetHero() {
         // Back to starting position
         this.y = this.startY;
@@ -116,7 +115,7 @@ const player = new Hero();
 const enemy_bug1 = new Enemy(-101, 0, 200);
 const enemy_bug2 = new Enemy(-101, 83, 300);
 const enemy_bug3 = new Enemy((-101 * 2.5), 83, 350);
-const enemy_bug4 = new Enemy(this.resetPosition, 166, 235);
+const enemy_bug4 = new Enemy(-101, 166, 235);
 
 // Init allEnemies array
 // For each enemy create and push new Enemy object into array
