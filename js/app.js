7ac2eb0c8937ff3ff1ac22a7e58a85d3bd4415c9
Mario Burgos <mario.burgos@jpl.nasa.gov>
2018-09-19 12:07:33 -0700

    Enemy Class using 'real' class syntax.

--- js/app.js
+++ js/app.js
@@ -1,39 +1,32 @@
-// Enemies our player must avoid
-var Enemy = function(x, y, speed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-    this.x = x;
-    this.y = y + 55;
-    this.speed = speed;
-    this.sprite = 'images/enemy-bug.png';
-    this.step = 101;
-    this.boundary = this.step * 5;
-    this.resetPosition = -this.step;
-};
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-
-    // If enemy is not past boundary, then keep moving
-    if(this.x < this.boundary) { // places bug just off screen
-        // Move forward
-        // Increment x by speed * dt
-        this.x += this.speed * dt;
+// Enemies our player must avoid - Now a 'real Class'
+class Enemy {
+    constructor(x, y, speed) {
+        // Variables applied to each of our instances
+        this.x = x;
+        this.y = y + 55;
+        this.speed = speed;
+        this.sprite = 'images/enemy-bug.png';
+        this.step = 101;
+        this.boundary = this.step * 5;
+        this.resetPosition = -this.step;
     }
-    else {
-        // Reset position to start
-        this.x = this.resetPosition;
-    }
-};
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+    update(dt) {
+        // If enemy is not past boundary, then keep moving
+        if(this.x < this.boundary) { // places bug just off screen
+            // Move forward, increment x by speed * dt
+            this.x += this.speed * dt;
+        }
+        else {
+            // Reset position to start
+            this.x = this.resetPosition;
+        }
+    }
+
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+}
 
 // Player class. This class requires an update(), render() and
 // a handleInput() method.
