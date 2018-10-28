c1ddae86ca17fbf8de359af248b76e118ba6172a
Rashad J Madison <rashadmad@gmail.com>
2018-10-28 09:10:14 -0500

    Changed indentation

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-
 //lane choices for bug postioning
 const FIRSTLANESPAWN = 240;
 const SECONDLANESPAWN = 160;
@@ -34,11 +33,11 @@ class Enemy {
   currentColumn() {
 
 
-      //column choices
-      const FIRSTCOLUMN = this.x >= 4 && this.x <= 101;
-      const SECONDCOLUMN = this.x >= 102 && this.x <= 200;
-      const THIRDCOLUMN = this.x >= 201 && this.x <= 298;
-      const FOURTHCOLUMN = this.x >= 300 && this.x <= 398;
+    //column choices
+    const FIRSTCOLUMN = this.x >= 4 && this.x <= 101;
+    const SECONDCOLUMN = this.x >= 102 && this.x <= 200;
+    const THIRDCOLUMN = this.x >= 201 && this.x <= 298;
+    const FOURTHCOLUMN = this.x >= 300 && this.x <= 398;
 
     let result = "";
     if (FIRSTCOLUMN) {
@@ -99,163 +98,162 @@ class Character {
   /*constructor*/
   constructor(sprite = boy) {
     //allows me to save postioning for latter
-      this.startingX = 200;
-      this.startingY = 400;
-      this.x = this.startingX;
-      this.y = this.startingY;
+    this.startingX = 200;
+    this.startingY = 400;
+    this.x = this.startingX;
+    this.y = this.startingY;
 
-      //charts how many hearts the player has
-      this.health = 4;
-      this.stonesCollected = 0;
+    //charts how many hearts the player has
+    this.health = 4;
+    this.stonesCollected = 0;
 
     // sprite
     this.sprite = sprite;
-  /*methods*/
-  //create character onscreen
-}
-
-takeDamage(){
-  this.respawn();
-
-  //lower health
-  this.health -= 1;
-  allHearts.pop();
-
-  //on player death
-  if(this.health <= 0){
-    headerText.textContent = "You Died";
-    modal.style.display = "block";
+    /*methods*/
+    //create character onscreen
   }
-}
 
-//reset position
-respawn(){
-  this.x = this.startingX;
-  this.y = this.startingY;
-}
+  takeDamage() {
+    this.respawn();
 
-currentColumn() {
+    //lower health
+    this.health -= 1;
+    allHearts.pop();
 
-  //column choices
-  const FIRSTCOLUMN = this.x >= 4 && this.x <= 101
-  const SECONDCOLUMN = this.x >= 102 && this.x <= 200
-  const THIRDCOLUMN = this.x >= 201 && this.x <= 298
-  const FOURTHCOLUMN = this.x >= 300 && this.x <= 398
-
-  let result = "";
-  if (FIRSTCOLUMN) {
-    result = 'firstColumn'
-  } else if (SECONDCOLUMN) {
-    result = 'secondColumn'
-  } else if (THIRDCOLUMN) {
-    result = 'thirdColumn'
-  } else if (FOURTHCOLUMN) {
-    result = 'fourthColumn'
-  } else {
-    result = 'fifthColumn'
-  }
-  return result;
-}
-
-currentLane() {
-
-  //row choices
-  const FIRSTLANE = this.y >= 80 && this.y <= 100;
-  const SECONDLANE = this.y >= 101 && this.y <= 180;
-  const THIRDLANE = this.y >= 181 && this.y <= 240;
-
-
-  let result = "";
-  if (FIRSTLANE) {
-    result = 'firstLane'
-  } else if (SECONDLANE) {
-    result = 'secondLane'
-  } else if (THIRDLANE) {
-    result = 'thirdLane'
-  }
-  return result;
-}
-
-render() {
-  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
-//movement
-handleInput(input) {
-//dead people dont move
-if(this.health > 0 && this.stonesCollected < 5){
-    let horizontalMovement = 98;
-    let lateralMovement = 80;
-
-    if (input === 'left') {
-      if (this.x > 5) {
-        this.x -= horizontalMovement;
-      }
-    } else if (input === 'up') {
-      if (this.y > 5) {
-        this.y -= lateralMovement;
-      }
-    } else if (input === 'right') {
-      if (this.x < 350) {
-        this.x += horizontalMovement;
-      }
-    } else if (input === 'down') {
-      if (this.y < 400) {
-        this.y += lateralMovement;
-      }
+    //on player death
+    if (this.health <= 0) {
+      headerText.textContent = "You Died";
+      modal.style.display = "block";
     }
   }
 
+  //reset position
+  respawn() {
+    this.x = this.startingX;
+    this.y = this.startingY;
+  }
 
-  //stone collections
-  if (this.x === stone.x && this.y === stone.y) {
-    //makes it so the stone will be placed in random locations
-    const xCoordinates = [4, 102, 298, 200, 396];
-    stone.x = xCoordinates[Math.floor(Math.random() * xCoordinates.length)];
+  currentColumn() {
+    //column choices
+    const FIRSTCOLUMN = this.x >= 4 && this.x <= 101
+    const SECONDCOLUMN = this.x >= 102 && this.x <= 200
+    const THIRDCOLUMN = this.x >= 201 && this.x <= 298
+    const FOURTHCOLUMN = this.x >= 300 && this.x <= 398
 
-    //change the location of the stone from on the river to on the grass
-    stone.y = stone.y === 0 ? 320 : 0;
+    let result = "";
+    if (FIRSTCOLUMN) {
+      result = 'firstColumn'
+    } else if (SECONDCOLUMN) {
+      result = 'secondColumn'
+    } else if (THIRDCOLUMN) {
+      result = 'thirdColumn'
+    } else if (FOURTHCOLUMN) {
+      result = 'fourthColumn'
+    } else {
+      result = 'fifthColumn'
+    }
+    return result;
+  }
 
-    //stone skins
-    const red = 'images/Gem Red.png';
-    const blue = 'images/Gem Blue.png';
-    const orange = 'images/Gem Orange.png';
-    const green = 'images/Gem Green.png';
-    const purple = 'images/Gem Purple.png';
+  currentLane() {
 
-    //this needs to cyle through infintely
-    //switches the color of the stone after collecting it
-    switch (stone.sprite) {
-      case blue:
-        stone.sprite = red;
-        this.stonesCollected += 1;
-        break;
-      case red:
-        stone.sprite = orange;
-        this.stonesCollected += 1;
-        break;
-      case orange:
-        stone.sprite = green;
-        this.stonesCollected += 1;
-        break;
-      case green:
-        stone.sprite = purple;
-        this.stonesCollected += 1;
-        break;
-      case purple:
-      //on win
-        this.stonesCollected += 1;
-        stone.sprite = blue;
-        stone.y = 0;
-      //display modal
+    //row choices
+    const FIRSTLANE = this.y >= 80 && this.y <= 100;
+    const SECONDLANE = this.y >= 101 && this.y <= 180;
+    const THIRDLANE = this.y >= 181 && this.y <= 240;
+
+
+    let result = "";
+    if (FIRSTLANE) {
+      result = 'firstLane'
+    } else if (SECONDLANE) {
+      result = 'secondLane'
+    } else if (THIRDLANE) {
+      result = 'thirdLane'
+    }
+    return result;
+  }
+
+  render() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
+  //movement
+  handleInput(input) {
+    //dead people dont move
+    if (this.health > 0 && this.stonesCollected < 5) {
+      let horizontalMovement = 98;
+      let lateralMovement = 80;
+
+      if (input === 'left') {
+        if (this.x > 5) {
+          this.x -= horizontalMovement;
+        }
+      } else if (input === 'up') {
+        if (this.y > 5) {
+          this.y -= lateralMovement;
+        }
+      } else if (input === 'right') {
+        if (this.x < 350) {
+          this.x += horizontalMovement;
+        }
+      } else if (input === 'down') {
+        if (this.y < 400) {
+          this.y += lateralMovement;
+        }
+      }
+    }
+
+
+    //stone collections
+    if (this.x === stone.x && this.y === stone.y) {
+      //makes it so the stone will be placed in random locations
+      const xCoordinates = [4, 102, 298, 200, 396];
+      stone.x = xCoordinates[Math.floor(Math.random() * xCoordinates.length)];
+
+      //change the location of the stone from on the river to on the grass
+      stone.y = stone.y === 0 ? 320 : 0;
+
+      //stone skins
+      const red = 'images/Gem Red.png';
+      const blue = 'images/Gem Blue.png';
+      const orange = 'images/Gem Orange.png';
+      const green = 'images/Gem Green.png';
+      const purple = 'images/Gem Purple.png';
+
+      //this needs to cyle through infintely
+      //switches the color of the stone after collecting it
+      switch (stone.sprite) {
+        case blue:
+          stone.sprite = red;
+          this.stonesCollected += 1;
+          break;
+        case red:
+          stone.sprite = orange;
+          this.stonesCollected += 1;
+          break;
+        case orange:
+          stone.sprite = green;
+          this.stonesCollected += 1;
+          break;
+        case green:
+          stone.sprite = purple;
+          this.stonesCollected += 1;
+          break;
+        case purple:
+          //on win
+          this.stonesCollected += 1;
+          stone.sprite = blue;
+          stone.y = 0;
+          //display modal
           modal.style.display = "block";
           headerText.textContent = "Congratulations You Win";
-      //puts player back to start
+          //puts player back to start
           this.respawn();
+      }
     }
   }
-}
 
-//collision system for our bugs
+  //collision system for our bugs
   update() {
     for (let enemy of allEnemies) {
 
@@ -265,6 +263,7 @@ if(this.health > 0 && this.stonesCollected < 5){
     }
   }
 }
+
 //symbols that represent if a character takes takeDamage
 class Heart {
   constructor(x, y) {
